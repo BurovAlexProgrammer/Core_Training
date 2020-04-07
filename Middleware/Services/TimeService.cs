b87333ca384208f962cbf2c1692b041cc87c9501
Alex <burovalexprogrammer@gmail.com>
2020-04-07 15:55:20 +0500

    Рассмотрел небольшой пример с Middleware

---
+++ Middleware/Services/TimeService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace _3_Middleware
+{
+    //Самый правильный подход, это Middleware
+    //TODO тема пока раскрыта не до конца, но суть видна
+    public class TimeMiddleware
+    {
+        public TimeMiddleware(RequestDelegate next)
+        {
+
+        }
+
+        public async Task InvokeAsync(HttpContext context, TimeService timeService)
+        {
+            await context.Response.WriteAsync(timeService.ShowTime());
+        }
+    }
+
+    //Пример правильной реализации слабых связей для сервиса
+    //В данном случае TimeService может быть определен как любой из классов, реализовавших интерфейс ITimeService
+    public class TimeService
+    {
+        ITimeSender _sender;
+        public TimeService(ITimeSender sender)
+        {
+            _sender = sender;
+        }
+
+        public string ShowTime()
+        {
+            return _sender.GetTime();
+        }
+    }
+
+
+    public interface ITimeSender
+    {
+        string GetTime();
+    }
+
+    class FullTimeSender : ITimeSender
+    {
+        public string GetTime()
+        {
+            return DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+
+    class ShortTimeSender : ITimeSender
+    {
+        public string GetTime()
+        {
+            return DateTime.Now.ToString("hh:mm");
+        }
+    }
+}
---
+++ Middleware/Startup.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _3_Middleware
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ITimeSender, ShortTimeSender>();   //Здесь определяется конкретная реализация для текущего интерфейса
+            services.AddTransient<TimeService>();   //Здесь цепляется сам сервис
+        }
+
+
+        public void Configure(IApplicationBuilder app) //Такой способ позволяет не добавлять параметры при добавлении сервиса
+        {
+            app.UseMiddleware<TimeMiddleware>();
+        }
+    }
+}
